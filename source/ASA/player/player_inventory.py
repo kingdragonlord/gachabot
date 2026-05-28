1e878a75b0a664deccdbf3e5df89f3e7a7f0df3b
kingdragonlord <157989802+kingdragonlord@users.noreply.github.com>
2026-05-27 22:23:48 -0700

    Handle encumbrance confirmation popup during transfer all

--- source/ASA/player/player_inventory.py
+++ source/ASA/player/player_inventory.py
@@ -66,6 +66,8 @@ def transfer_all_inventory():
         logs.logger.debug(f"transfering all from our inventory into strucutre")
         time.sleep(0.2*settings.lag_offset)
         windows.click(variables.get_pixel_loc("transfer_all_inventory_x"),variables.get_pixel_loc("transfer_all_y"))
+        time.sleep(0.3*settings.lag_offset) # wait for the popup to appear
+        windows.click(variables.get_pixel_loc("accept_drop_all_x"),variables.get_pixel_loc("accept_drop_all_y"))
         time.sleep(0.1*settings.lag_offset)
 
 
--- source/ASA/strucutres/inventory.py
+++ source/ASA/strucutres/inventory.py
@@ -85,6 +85,8 @@ def transfer_all_from():
         logs.logger.debug(f"transfering all from object")
         time.sleep(0.2*settings.lag_offset)
         windows.click(variables.get_pixel_loc("transfer_all_from_x"), variables.get_pixel_loc("transfer_all_y"))
+        time.sleep(0.3*settings.lag_offset) # wait for the popup to appear
+        windows.click(variables.get_pixel_loc("accept_drop_all_x"),variables.get_pixel_loc("accept_drop_all_y"))
         time.sleep(0.1*settings.lag_offset)
     else:
         from source.utility.exceptions import TaskFailedException
